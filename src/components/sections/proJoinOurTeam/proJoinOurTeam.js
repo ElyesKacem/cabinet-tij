e01d615f269b17f9262afc77c1979991d0da8b52
elyes <elyeskacemeducation@gmail.com>
2023-03-04 17:22:28 +0100

    finishing home , moving to calendar

--- src/components/sections/proJoinOurTeam/proJoinOurTeam.js
+++ src/components/sections/proJoinOurTeam/proJoinOurTeam.js
@@ -4,28 +4,52 @@ import LeftRightButton from '../../buttons/leftRightButton/leftRightButton'
 import "./proJoinOurTeam.css"
 import Background from "../../../assets/images/meeting.png"
 import VerticalBar from '../../verticalBar/verticalBar'
+import Media from 'react-media'
 
 export default function ProJoinOurTeam() {
+  const GLOBAL_MEDIA_QUERIES = {
+    large: "(min-width: 816px)",
+    medium: "(max-width: 816px)",
+  };
   return (
-    //     <div className='proJoinOurTeam-wrapper'>
 
-    //       <div className="proJoinOurTeam-content">
-    //         <div className='proJoinOurTeam-content-header'>Professsional ? Join Our team</div>
-    //         <div className='proJoinOurTeam-content-paragraph'>join our community and be a part of something special .</div>
-    //         <div className='proJoinOurTeam-content-paragraph'>Together, we can make a difference and create meaningful change.</div>
-    //         <LeftRightButton title="Career" style={{transform:'translateY(40px)'}}/>
-    //       </div>
-    // <div className="proJoinOurTeam-overlay"></div>
-    //     </div>
+
     <BackgroundImage style={{ height: 550 }} img={Background} rgba1="0,0,0" rgba2="0,0,0" opacity="0.3">
       <div className="proJoinOurTeam-content">
         <VerticalBar top="100px" left="50px" />
-            <VerticalBar top="200px" left="70%" />
-            <VerticalBar top="400px" left="90%" />
-        <div className='proJoinOurTeam-content-header'>Professsional ? Join Our team</div>
-        <div className='proJoinOurTeam-content-paragraph'>join our community and be a part of something special .</div>
-        <div className='proJoinOurTeam-content-paragraph'>Together, we can make a difference and create meaningful change.</div>
-        <LeftRightButton title="Career" style={{ transform: 'translateY(40px)' }} />
+        <VerticalBar top="200px" left="70%" />
+        <VerticalBar top="400px" left="90%" />
+
+
+        <Media queries={GLOBAL_MEDIA_QUERIES}>
+          {matches => (
+            <React.Fragment>
+              {matches.large &&
+                <>
+                  <div className='proJoinOurTeam-content-header'>Professsional ? <br /> Join Our team</div>
+                  <div className='proJoinOurTeam-content-paragraph'>join our community and be a part of something special .</div>
+                  <div className='proJoinOurTeam-content-paragraph'>Together, we can make a difference and create meaningful change.</div>
+                  <LeftRightButton title="Career" style={{ transform: 'translateY(30px)' }} />
+                </>
+              }
+              {matches.medium &&
+              <>
+                <div style={{transform:"scale(0.7)"}}>
+                  <div className='proJoinOurTeam-content-header' style={{whiteSpace: "nowrap"}}>Professsional ? <br /> Join Our team</div><br />
+                  <div className='proJoinOurTeam-content-paragraph'>join our community and be a part of something special .</div>
+                  <div className='proJoinOurTeam-content-paragraph'>Together, we can make a difference and create meaningful change.</div>
+                </div>
+                <LeftRightButton title="Career" />
+              </>
+              }
+            </React.Fragment>
+          )
+          }
+        </Media>
+
+
+
+        
       </div>
     </BackgroundImage>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,12 +13,12 @@ export default function Home() {
     <div>
       
 
-      {/* <VideoSection />
+      <VideoSection />
       <Services />
       <WhyChoiceUs />
       <DoYouNeedMoreAssistance />
       <OurServices/>
-      <GetInTouchWithUs /> */}
+      <GetInTouchWithUs />
       <ProJoinOurTeam />
       
     </div>
