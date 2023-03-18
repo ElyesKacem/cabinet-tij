f5bbea2090bd8e713113e0771e85681734c8d7ee
hatem782 <hatembenechikh100@gmail.com>
2023-03-18 17:24:58 +0100

    ss

--- src/components/footerText/footerText.js
+++ src/components/footerText/footerText.js
@@ -1,51 +1,49 @@
-import React from 'react'
-import "./footerText.css"
-import EastIcon from '@mui/icons-material/East';
+import React from "react";
+import "./footerText.css";
+import EastIcon from "@mui/icons-material/East";
 
 export default function FooterText(props) {
-    return (
-        <div className='FooterText'>
-            <div className='footerText-title'>
-                <div style={{transform:"translate(-10px,5px)"}}>
-                    <EastIcon />
-                </div>
-                <div className='footerText-title'>
-                    &nbsp;&nbsp;&nbsp;{props.title}
-                </div>
-            </div>
-            <br />
-            <div className='footerText-content'>
-                {props.content}
-                {props.title=="Solutions" && <>
-                
-                Interpreting 
-                <br />
-                <br />
-                Bussiness 
-                <br />
-                <br />
-                Certified Translation 
-                <br />
-                <br />
-                Sworn Translation
-                <br />
-                <br />
-                Personal Services 
-                <br />
-                <br />
-                </>}
-                {props.title=="Information" && <>
-                
-                contact@cabinet-tij.com
-                <br />
-                <br />
-                06 99 08 64 91 
-                <br />
-                <br />
-                1 rue René et Isa Lefèvre 93450 L'IIe-Saint-Denis
-    
-                </>}
-            </div>
+  return (
+    <div className="FooterText">
+      <div className="footerText-title">
+        <div style={{ transform: "translate(-10px,5px)" }}>
+          <EastIcon />
         </div>
-    )
+        <div className="footerText-title">&nbsp;&nbsp;&nbsp;{props.title}</div>
+      </div>
+      <br />
+      <div className="footerText-content">
+        {props.content}
+        {props.title == "Solutions" && (
+          <>
+            Interpreting
+            <br />
+            <br />
+            Bussiness
+            <br />
+            <br />
+            Certified Translation
+            <br />
+            <br />
+            Sworn Translation
+            <br />
+            <br />
+            Personal Services
+            <br />
+            <br />
+          </>
+        )}
+        {props.title == "Information" && (
+          <>
+            contact@cabinet-tij.com
+            <br />
+            <br />
+            06 99 08 64 91
+            <br />
+            <br />1 rue René et Isa Lefèvre 93450 L'IIe-Saint-Denis
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
