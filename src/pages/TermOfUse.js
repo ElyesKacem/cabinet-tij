942f87ce4d2c8cb8cf71f5a6dc2af5773aec4881
elyes <elyeskacemeducation@gmail.com>
2023-03-25 17:28:59 +0100

    term of use translating

--- src/pages/TermOfUse.js
+++ src/pages/TermOfUse.js
@@ -1,11 +1,52 @@
 import React from "react";
 import DownArrow from "../assets/images/DownArrow.png";
+import { LangContext } from "../context/Lang.context";
 
 export default function TermOfUse() {
+  const data = {
+    fr: {
+      h1: "Politique de Confidentialité :",
+      parag:
+        "TIJ s'engage à protéger la confidentialité des données personnelles de ses clients et de ses utilisateurs. Les informations collectées sont utilisées pour répondre à leurs demandes et pour améliorer l'expérience utilisateur. Nous ne divulguons aucune information à des tiers, sauf dans les cas où cela est exigé par la loi ou les autorités compétentes.",
+      h2: "Motions légales :",
+      t1: "Nom de la société :",
+      t2: "Adresse siège Social : ",
+      t3: "Numéro de telephone ",
+      t4: "Adresse e-mail ",
+      t5: "Numéro d’immatriculation ",
+      t6: "Directeur",
+    },
+    en: {
+      h1: "Privacy Policy",
+      parag:
+        "TIJ is committed to protecting the privacy of its customers and users' personal data. The information collected is used to respond to their requests and to improve the user experience. We do not disclose any information to third parties, except where required by law or competent authorities",
+      h2: "Terms of Use :",
+      t1: "Company name :",
+      t2: "Registered address :",
+      t3: "Phone number :",
+      t4: "Email address :",
+      t5: "Registration number :",
+      t6: "Director :",
+    },
+  };
+
+  const { lang } = React.useContext(LangContext);
+  if (!lang) {
+    // return texts.fr;
+  } else {
+    // return texts[lang];
+  }
   const blueTitle = {
     fontSize: 20,
     color: "#2f49e7",
   };
+
+  const [text, setText] = React.useState(data["fr"]);
+
+  React.useEffect(() => {
+    setText(data[lang]);
+  }, [lang]);
+
   return (
     <div
       id="termeofuse"
@@ -21,7 +62,7 @@ export default function TermOfUse() {
       <br />
       <br />
       <div style={{ marginLeft: 50 }}>
-        <div className="EnrollementImage-title">Privacy Policy</div>
+        <div className="EnrollementImage-title">{text.h1}</div>
         <br />
         <br />
         <img
@@ -32,42 +73,31 @@ export default function TermOfUse() {
         <br />
         <br />
         <br />
-        <p style={{ color: "white" }}>
-          TIJ is committed to protecting the privacy of its customers and users'
-          personal data. The information collected is used to respond to their
-          requests and to improve the user experience. We do not disclose any
-          information to third parties, except where required by law or
-          competent authorities
-        </p>
-        <div className="EnrollementImage-title">Term of use</div>
+        <p style={{ color: "white" }}>{text.parag}</p>
+        <div className="EnrollementImage-title">{text.h2}</div>
         <p style={{ color: "white" }}>
           {" "}
-          <b style={blueTitle}>Company name : </b> TIJ{" "}
+          <b style={blueTitle}>{text.t1} </b> TIJ{" "}
         </p>
         <p style={{ color: "white" }}>
           {" "}
-          <b style={blueTitle}>Registered address : </b> 3 rue Boccador, 75008
-          Paris.{" "}
+          <b style={blueTitle}>{text.t2} </b> 3 rue Boccador, 75008 Paris.{" "}
         </p>
         <p style={{ color: "white" }}>
           {" "}
-          <b style={blueTitle}>Phone number : </b> 0033 06 99 08 64 91{" "}
+          <b style={blueTitle}>{text.t3} </b> 0033 06 99 08 64 91{" "}
         </p>
         <p style={{ color: "white" }}>
           {" "}
-          <b style={blueTitle}>Registered address : </b> 3 rue Boccador, 75008
+          <b style={blueTitle}>{text.t4} </b> contact@cabinet-tij.com
         </p>
         <p style={{ color: "white" }}>
           {" "}
-          <b style={blueTitle}>Email address : </b> contact@cabinet-tij.com
+          <b style={blueTitle}>{text.t5} </b> 90274422600017
         </p>
         <p style={{ color: "white" }}>
           {" "}
-          <b style={blueTitle}>Registration number : </b> 90274422600017
-        </p>
-        <p style={{ color: "white" }}>
-          {" "}
-          <b style={blueTitle}>Director : </b> Manel BOUABIDI
+          <b style={blueTitle}>{text.t6} </b> Manel BOUABIDI
         </p>
       </div>
       <br />
